3d7b22dff299d1cc5100c4693e7d363de8bcce62
Marko Bakovic <markobakovic95@gmail.com>
2018-01-11 22:52:09 +0000

    template

---
+++ _template.cpp
@@ -0,0 +1,31 @@
+/*
+    Checker template.
+*/
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Example, but you don't have to use streams!
+    ifstream outFile(argv[1]);  // User output
+    ifstream solFile(argv[2]);  // Expected output
+    ifstream inFile(argv[3]);  // Input
+
+    // Check solution
+
+    outFile.close();
+    solFile.close();
+    inFile.close();
+
+    return 0;  // For incorrect
+    return 1;  // For correct
+}
